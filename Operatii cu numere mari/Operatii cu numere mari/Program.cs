ac8fc448459d8690dfea7f6fa737f58aec54061f
MeguRobi <megurobi14@outlook.hu>
2020-12-08 10:29:24 +0200

    division is modified a little bit

--- Operatii cu numere mari/Operatii cu numere mari/Program.cs
+++ Operatii cu numere mari/Operatii cu numere mari/Program.cs
@@ -29,6 +29,64 @@ namespace Operatii_cu_numere_mari
             Console.WriteLine();
         }
 
+        private static void Write()
+        {
+            Console.WriteLine("Acesta este un program care poate efectua operatii cu numere mari.");
+            Console.WriteLine("Operatiile valide sunt:");
+            Console.WriteLine("+   adaugare");
+            Console.WriteLine("-   scadere");
+            Console.WriteLine("*   inmultire");
+            Console.WriteLine("/   impartie");
+            Console.WriteLine("p   ridicare la putere");
+            Console.WriteLine("r   radacina patrata       (Coming soon) ");
+            Console.WriteLine("f   factorial");
+        }
+
+        private static string ValidInputNumber()
+        {
+            string onlyNumbers = @"^\d+$";
+            string line;
+            Match result;
+            do
+            {
+                line = Console.ReadLine();
+                result = Regex.Match(line, onlyNumbers);
+            } while (!result.Success);
+
+            return result.Value;
+        }
+
+        private static int[] MakeVectorFrom(string bigNumber)
+        {
+            v = new int[bigNumber.Length];
+            for (int i = 0; i < v.Length; i++)
+            {
+                v[i] = Convert.ToInt32(bigNumber[i] - '0');
+            }
+            return v;
+        }
+
+        private static int[] MakeVectorFrom(int num)
+        {
+            if (num == 0) return null;
+            int lenght = 0;
+            int aux = num;
+            int[] vtemp;
+            while (aux != 0)
+            {
+                lenght++;
+                aux /= 10;
+            }
+            vtemp = new int[lenght];
+            while (num != 0)
+            {
+                vtemp[--lenght] = num % 10;
+                num /= 10;
+            }
+
+            return vtemp;
+        }
+
         private static void Switch_And_Do(char operation)
         {
             if (operation == 'f')
@@ -69,47 +127,184 @@ namespace Operatii_cu_numere_mari
             
         }
 
-        private static void SquareRoot()
+        /*************************************************************
+         *                        OPERATIONS
+         * ***********************************************************/
+        private static void Addition()
         {
-            Console.WriteLine("****** Radacina patrata al numarului ******");
-            
+            Console.WriteLine("****** suma celor doua numere ******");
+            View(Suma(v1, v2));
         }
 
-        private static void Power()
+        private static int[] Suma(int[] v1, int[] v2)
         {
-            Console.WriteLine("****** Puterea celor doua numere ******");
-            View(Putere(v1, v2));
+            if (v1.Length < v2.Length)
+            {
+                return Suma(v2, v1);
+            }
+            int max, k;
+            max = v1.Length;
+            k = v2.Length;    //contorul pentru v2
+            int[] v = new int[max + 1];
+            int[] vtemp = new int[max];
+            int trans = 0;
+            for (int i = max - 1; i >= 0; i--)
+            {
+                k--;
+                v[i + 1] = trans + v1[i];     //adaugam transportul acumulat din pozitia anterioara 
+                                              // si elementele vectorului 1
+                if (k >= 0)
+                    v[i + 1] += v2[k];      //adaugam elementele vectorului 2 pana cand avem ce adauga :) (Exclude overflow)     
+
+                trans = v[i + 1] / 10; //transportul va fi intotdeauna egal cu cifra zecilor
+                v[i + 1] %= 10;        //eliminam valoarea transportului din pozitia curenta a vetorului
+            }
+            if (trans == 0)
+            {
+                for (int i = 1; i < v.Length; i++)
+                {
+                    vtemp[i - 1] = v[i];
+                }
+                v = vtemp;
+                //daca transportul final este 0 trebuie eliminat 0-ul de la inceputul vectorului
+                //si lungimea vectorului devine mai mic cu 1 
+            }
+            else
+            {
+                v[0] = trans;   //adaugam ultimul transport, daca nu este 0
+            }
+            return v;
+        }
+
+        private static void Subtraction()
+        {
+            Console.WriteLine("****** Diferenta celor doua numere ******");
+            int[] dif = Diferenta(v1, v2);
+            if (isNegative)
+            {
+                Console.Write("-");
+            }
+            View(dif);
+        }
+
+        private static int[] Diferenta(int[] v1, int[] v2)
+        {
+
+            if (!PrimulEsteMaiMare(v1, v2))
+            {
+                isNegative = true;
+                return Diferenta(v2, v1);
+            }
+
+            int max, min;
+            max = v1.Length;
+            min = v2.Length;
+            int k = min;    //contorul pentru v2
+            int[] v = new int[max];
+            int[] vtemp;
+            for (int i = max - 1; i >= 0; i--)
+            {
+                k--;
+                if (k >= 0)
+                {
+                    v[i] = v1[i] - v2[k];
+                }
+                else
+                {
+                    v[i] = v1[i];
+                }
+                if (v[i] < 0)
+                {
+                    v1[i - 1]--;  //imprumut
+                    v[i] += 10;
+                }
+            }
+
+            /******** eliminarea 0-urilor de la inceputul vectorului ********/
+            while (v.Length > 1 && v[0] == 0)
+            {
+                vtemp = new int[--max];
+                for (int i = 1; i < v.Length; i++)
+                {
+                    vtemp[i - 1] = v[i];
+                }
+                v = vtemp;
+            }
+            return v;
+        }
+
+        private static void Multiplication()
+        {
+            Console.WriteLine("****** Produsul celor doua numere ******");
+            View(InmultireVectori(v1, v2));
+        }
+
+        private static int[] InmultireVectori(int[] v1, int[] v2)
+        {
+            int j = 1, scalar = v2[v2.Length - 1];
+            int[] vSum = InmultireCuScalar(v1, scalar);
+            for (int i = v2.Length - 2; i >= 0; i--)
+            {
+                scalar = v2[i];
+                vSum = Suma(vSum, AdaugaNumar(InmultireCuScalar(v1, scalar), 0, j));
+                j++;
+            }
+            return vSum;
+        }
+
+        private static int[] InmultireCuScalar(int[] v1, int scalar)
+        {
+            int[] v;
+            if (scalar != 0)
+            {
+                v = v1;
+                for (int i = 1; i < scalar; i++)
+                {
+                    v = Suma(v, v1);
+                }
+            }
+            else
+            {
+                v = new int[1];
+                v[0] = 0;
+            }
+            return v;
         }
 
         private static void Division()
         {
-            rest = null;
+            rest = null;v = null;
             v = Impartire(v1, v2);
             int nrZecimale=0;
-            if (v!=null)
+            if (v == null && rest == null)
+            {
+                Console.WriteLine("Nu putem divide cu 0!");
+                Console.WriteLine("Incearca cu un alt divizor");
+                Console.WriteLine();
+                Switch_And_Do(operation);
+            }
+            else
             {
                 if (rest != null)
                 {
                     Console.WriteLine("Cu ce aproximare sa fie calculata impartirea?");
                     Console.Write("Numarul maxim a zecimalelor:");
-                    nrZecimale = int.Parse(Console.ReadLine());
+                    nrZecimale = int.Parse(ValidInputNumber());
+
                 }
 
                 Console.WriteLine("****** Catul impartiri ******");
-                View(v);
+                if (v != null)
+                {
+                    View(v);
+                }
+                if (v == null) Console.Write("0");
                 if (nrZecimale != 0)
                 {
                     Console.Write(".");
                     v = Decimal(rest, v2, nrZecimale);
                     View(v);
                 }
-            }
-            else
-            {
-                Console.WriteLine("Nu putem divide cu 0!");
-                Console.WriteLine("Incearca cu un alt divizor");
-                Console.WriteLine();
-                Switch_And_Do(operation);
             }
         }
 
@@ -189,55 +384,56 @@ namespace Operatii_cu_numere_mari
                 return v;
             }
         }
-       
-        private static void Multiplication()
+
+        private static void Power()
         {
-            Console.WriteLine("****** Produsul celor doua numere ******");
-            View(InmultireVectori(v1, v2));
+            Console.WriteLine("****** Puterea celor doua numere ******");
+            View(Putere(v1, v2));
         }
 
-        private static void Subtraction()
+        private static int[] Putere(int[] v1, int[] v2)
         {
-            Console.WriteLine("****** Diferenta celor doua numere ******");
-            int[] dif = Diferenta(v1, v2);
-            if (isNegative)
+            int[] vPutere = PutereScalar(v1, v2[v2.Length - 1]);
+            int scalar, j = 1;
+
+            for (int i = v2.Length - 2; i >= 0; i--)
             {
-                Console.Write("-");
+                scalar = v2[i];
+                vPutere = InmultireVectori(vPutere, PutereScalar(v1, scalar * (int)Math.Pow(10, j)));
+                j++;
             }
-            View(dif);
+            return vPutere;
         }
 
-        private static void Addition()
+        private static int[] PutereScalar(int[] v1, int n)
         {
-            Console.WriteLine("****** suma celor doua numere ******");
-            View(Suma(v1, v2));
-        }
-
-        private static void Write()
-        {
-            Console.WriteLine("Acesta este un program care poate efectua operatii cu numere mari.");
-            Console.WriteLine("Operatiile valide sunt:");
-            Console.WriteLine("+   adaugare");
-            Console.WriteLine("-   scadere");
-            Console.WriteLine("*   inmultire");
-            Console.WriteLine("/   impartie");
-            Console.WriteLine("p   ridicare la putere");
-            Console.WriteLine("r   radacina patrata       (Coming soon) ");
-            Console.WriteLine("f   factorial");
-        }
-
-        private static string ValidInputNumber()
-        {
-            string onlyNumbers = @"^\d+$";
-            string line;
-            Match result;
-            do
+            int[] putere;
+            if (n == 0)
             {
-                line = Console.ReadLine();
-                result = Regex.Match(line, onlyNumbers);
-            } while (!result.Success);
+                putere = new int[1];
+                putere[0] = 1;
+            }
+            else
+            {
+                putere = v1;
+            }
+
+            for (int i = 2; i <= n; i++)
+            {
+                putere = InmultireVectori(putere, v1);
+            }
+            return putere;
+        }
+
+        private static void SquareRoot()
+        {
+            Console.WriteLine("****** Radacina patrata al numarului ******");
+            RadacinaPatrata(v1);
+        }
+
+        private static void RadacinaPatrata(int[] v1)
+        {
             
-            return result.Value;
         }
 
         private static void Fact()
@@ -288,110 +484,9 @@ namespace Operatii_cu_numere_mari
             return fact;
         }
 
-        private static int[] MakeVectorFrom(int num)
-        {
-            if (num == 0) return null;
-            int lenght = 0;
-            int aux = num;
-            int[] vtemp;
-            while (aux != 0)
-            {
-                lenght++;
-                aux /= 10;
-            }
-            vtemp = new int[lenght];
-            while (num != 0)
-            {
-                vtemp[--lenght] = num % 10;
-                num /= 10;
-            }
-            
-            return vtemp;
-        }
-
-        private static int[] Putere(int[] v1, int[] v2)
-        {
-            int[] vPutere = PutereScalar(v1, v2[v2.Length-1]);
-            int scalar, j = 1;
-            
-            for (int i = v2.Length - 2; i >= 0; i--)
-            {
-                scalar = v2[i];
-                vPutere = InmultireVectori(vPutere, PutereScalar(v1, scalar * (int)Math.Pow(10,j)));
-                j++;
-            }
-            return vPutere;
-        }
-
-        private static int[] PutereScalar(int[] v1, int n)
-        {
-            int[] putere;
-            if (n==0)
-            {
-                putere = new int[1];
-                putere[0] = 1;
-            }
-            else
-            {
-                putere = v1;
-            }
-            
-            for (int i = 2; i <= n; i++)
-            {
-                putere = InmultireVectori(putere, v1);
-            }
-            return putere;
-        }
-
-        private static int[] InmultireVectori(int[] v1, int[] v2)
-        {
-            int j = 1, scalar = v2[v2.Length - 1];
-            int[] vSum = InmultireCuScalar(v1, scalar ); 
-            for (int i = v2.Length - 2; i >= 0; i--)
-            {
-                scalar = v2[i]; 
-                vSum = Suma(vSum , AdaugaNumar(InmultireCuScalar(v1, scalar),0 , j));
-                j++;
-            }
-            return vSum;
-        }
-
-        private static int[] AdaugaNumar(int[] v1, int numar, int decateori)
-        {
-            int[] vtemp = new int[v1.Length + decateori];
-            for (int i = 0; i < vtemp.Length; i++)
-            {
-                if (i < v1.Length)
-                {
-                    vtemp[i] = v1[i];
-                }
-                else
-                {
-                    vtemp[i] = numar;
-                }
-            }
-            return vtemp;
-        }
-
-        private static int[] InmultireCuScalar(int[] v1, int scalar)
-        {
-            int[] v;
-            if (scalar!=0)
-            {
-                v = v1;
-                for (int i = 1; i < scalar; i++)
-                {
-                    v = Suma(v, v1);
-                }
-            }
-            else
-            {
-                v = new int[1];
-                v[0] = 0;
-            }
-            return v;
-        }
-
+        /*************************************************************
+         *                        LOGICALS & OTHERS
+         * ***********************************************************/
         private static bool PrimulEsteMaiMare( int[] v1, int[] v2)
         {
             if (v1.Length < v2.Length) return false;
@@ -412,89 +507,21 @@ namespace Operatii_cu_numere_mari
             return true;
         }
 
-        private static int[] Diferenta(int[] v1, int[] v2)
+        private static int[] AdaugaNumar(int[] v1, int numar, int decateori)
         {
-          
-            if (!PrimulEsteMaiMare(v1, v2))
+            int[] vtemp = new int[v1.Length + decateori];
+            for (int i = 0; i < vtemp.Length; i++)
             {
-                isNegative = true;
-                return Diferenta(v2, v1);
-            }
-            
-            int max,min;
-            max = v1.Length;
-            min = v2.Length;    
-            int k = min;    //contorul pentru v2
-            int[] v = new int[max];
-            int[] vtemp;
-            for (int i = max - 1; i >= 0; i--)
-            {
-                k--;
-                if (k >= 0)
+                if (i < v1.Length)
                 {
-                    v[i] = v1[i] - v2[k];
+                    vtemp[i] = v1[i];
                 }
                 else
                 {
-                    v[i] = v1[i];
-                }
-                if (v[i] < 0)
-                {
-                    v1[i - 1]--;  //imprumut
-                    v[i] += 10;
+                    vtemp[i] = numar;
                 }
             }
-        
-            /******** eliminarea 0-urilor de la inceputul vectorului ********/
-            while (v.Length>1 && v[0]==0)
-            {
-                vtemp = new int[--max];
-                for (int i = 1; i < v.Length; i++)
-                {
-                    vtemp[i - 1] = v[i];
-                }
-                v = vtemp;
-            }
-            return v;
-        }
-        private static int[] Suma(int[] v1, int[] v2)
-        {
-            if (v1.Length < v2.Length)
-            {
-                return Suma(v2, v1);
-            }
-            int max, k;
-            max = v1.Length;
-            k = v2.Length;    //contorul pentru v2
-            int[] v = new int[max + 1];
-            int[] vtemp = new int[max];
-            int trans = 0;
-            for (int i = max - 1; i >= 0; i--)
-            {
-                k--;
-                v[i + 1] = trans+v1[i];     //adaugam transportul acumulat din pozitia anterioara 
-                                            // si elementele vectorului 1
-                if (k >= 0)
-                    v[i + 1] += v2[k];      //adaugam elementele vectorului 2 pana cand avem ce adauga :) (Exclude overflow)     
-                                       
-                trans = v[i + 1] / 10; //transportul va fi intotdeauna egal cu cifra zecilor
-                v[i + 1] %= 10;        //eliminam valoarea transportului din pozitia curenta a vetorului
-            } 
-            if (trans==0)
-            {
-                for (int i = 1; i < v.Length; i++)
-                {
-                    vtemp[i - 1] = v[i];
-                }
-                v = vtemp;
-                //daca transportul final este 0 trebuie eliminat 0-ul de la inceputul vectorului
-                //si lungimea vectorului devine mai mic cu 1 
-            }
-            else
-            {
-                v[0] = trans;   //adaugam ultimul transport, daca nu este 0
-            }
-            return v;
+            return vtemp;
         }
 
         private static void View(int[] v)
@@ -505,14 +532,5 @@ namespace Operatii_cu_numere_mari
             }
         }
 
-        private static int[] MakeVectorFrom(string bigNumber)
-        {
-            v = new int[bigNumber.Length];
-            for (int i = 0; i < v.Length; i++)
-            {
-                v[i] = Convert.ToInt32(bigNumber[i]-'0');
-            }
-            return v;
-        }
     }
 }
